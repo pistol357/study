6c6a5556a5faf5719b2ee5a6241e22831a9dd2c3
pistol357 <jeonghan615@gmail.com>
2026-09-03 16:10:10 +0900

    [Feat] GetKey로 space를 입력 받아 날개를 돌리고 회전 속도가 일정 수치 이상일 경우 비행 시키는 코드 작성

--- 260902_study/Assets/Scripts/SpinWings.cs
+++ 260902_study/Assets/Scripts/SpinWings.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class SpinWings : MonoBehaviour
 {
+    [SerializeField] private GameObject _helicopter;
+    [SerializeField] private float _spinSpeed;
+
+    private void Update()
+    {
+        Read();
+        Spin();
+        Fly();
+    }
+
+    private void Read()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _spinSpeed++;
+        }
+    }
+
     private void Spin()
     {
+        transform.Rotate(Vector3.up, _spinSpeed * Time.deltaTime);
+    }
 
+    private void Fly()
+    {
+        if(_spinSpeed >= 1000)
+        {
+            Vector3 position = _helicopter.transform.position;
+            position.y = _spinSpeed / 100;
+            _helicopter.transform.position = position;
+        }
     }
 }
