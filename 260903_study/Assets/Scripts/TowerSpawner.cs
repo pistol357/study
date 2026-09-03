d02b339019c5239475d84cda58e0b955518a0443
pistol357 <jeonghan615@gmail.com>
2026-09-03 14:58:22 +0900

    [Feat] 부모 아래로 정리하기

--- 260903_study/Assets/Scripts/TowerSpawner.cs
+++ 260903_study/Assets/Scripts/TowerSpawner.cs
@@ -6,6 +6,7 @@ public class TowerSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _towerPrefab;
     private int _count;
+    [SerializeField] private Transform _spawnRoot;
 
     private void Update()
     {
@@ -23,7 +24,7 @@ public class TowerSpawner : MonoBehaviour
     private void SpawnOne()
     {
         Vector3 spawnPosition = new Vector3(_count * 2f, 0f, 0f);
-        Instantiate(_towerPrefab, spawnPosition, Quaternion.identity);
+        Instantiate(_towerPrefab, spawnPosition, Quaternion.identity, _spawnRoot);
         _count += 1;
         Debug.Log($"TowerSpawner: {_count}번째를 만들었습니다.");
     }
